acc956aee00089b9f755ff50935d5080edcdd3c0
Adam Risberg <myniztan@hotmail.com>
2018-03-01 02:13:17 -0600

    Add index view, handlebars, and readme

---
+++ README.md
@@ -0,0 +1,7 @@
+Timestamp Microservice converts and returns natural dates and unix timestamps to both formats in JSON.
+
+To use this service, include a natural date or unix timestamp at the end of the url.
+
+Examples:
+/January 1, 2015
+/1420092000
--- index.js
+++ index.js
@@ -3,12 +3,14 @@ var dateConverter = require("./utils/date-converter");
 var app = express();
 var port = process.env.PORT || 3000;
 
+app.set("view engine", "hbs");
+
 app.get("/:date", function (req, res) {
   res.json(dateConverter(req.params.date));
 });
 
-app.get("/", function(req, res) {
-  res.send("HOME PAGE");
+app.get("/", function (req, res) {
+  res.render("index", { url: req.get("host") });
 });
 
 app.listen(port, function() {
